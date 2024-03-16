a63caee2b557b8018f0b8bcd1e8efe13524aeee3
deltaTreize <ludo.leblond13@gmail.com>
2024-03-16 19:23:25 +0100

    mise à jour de tous les projets

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -8,7 +8,6 @@ export function Navbar() {
       <nav className="links">
       <a href="https://github.com/deltaTreize?tab=repositories" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a> 
       <a href="https://www.linkedin.com/in/ludovic-leblond-00433675/" target="_blank" rel="noreferrer"> <i className="fa-brands fa-linkedin"></i></a>
-      <a href="https://www.instagram.com/ludovic.leblond.169/" target="_blank" rel="noreferrer"> <i className="fa-brands fa-instagram"></i></a>
       <a href="https://twitter.com/deltaTreize" target="_blank" rel="noreferrer"> <i className="fa-brands fa-twitter"></i></a>
       </nav>
 		</div>
--- src/pages/projetVisual/projetVisual.tsx
+++ src/pages/projetVisual/projetVisual.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import * as React from "react";
 
 import "./projetVisual.scss";
@@ -5,7 +6,6 @@ import "./projetVisual.scss";
 export interface props {
 	title: string;
 	imageAdresse: string;
-	imageAlt: string;
 	linkInternet: string;
 	linkGitHub: string;
 }
@@ -14,18 +14,18 @@ function ProjetVisual(props: props) {
 	return (
 		<div className="container-visual" key={props.title}>
 			<h3>{props.title}</h3>
-			<img src={props.imageAdresse} alt={props.imageAlt} />
+			<img src={props.imageAdresse} alt="visuel du site" />
 
 			<div className="linkInternet">
 				<i className="fa-solid fa-globe"></i>
-				<a href={props.linkInternet} className="site">
+				<a href={props.linkInternet} className="site" target="_blank">
 					visiter le site
 				</a>
 			</div>
 
 			<div className="linkGithub">
 				<i className="fa-brands fa-github"></i>
-				<a href={props.linkGitHub} className="github">
+				<a href={props.linkGitHub} className="github" target="_blank">
 					voir le code
 				</a>
 			</div>
