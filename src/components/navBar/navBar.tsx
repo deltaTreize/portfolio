b324c8326778c02a26ec2d717d18df05ba8dc664
deltaTreize <ludo.leblond13@gmail.com>
2024-03-16 19:24:21 +0100

    modification navbar pour ajout mail

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -9,6 +9,7 @@ export function Navbar() {
       <a href="https://github.com/deltaTreize?tab=repositories" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a> 
       <a href="https://www.linkedin.com/in/ludovic-leblond-00433675/" target="_blank" rel="noreferrer"> <i className="fa-brands fa-linkedin"></i></a>
       <a href="https://twitter.com/deltaTreize" target="_blank" rel="noreferrer"> <i className="fa-brands fa-twitter"></i></a>
+      <a href="mailto: ludo.leblond13@gmail.com"><i className="fa-solid fa-envelope" style={{color: "#ffffff"}}></i></a>
       </nav>
 		</div>
 	);
