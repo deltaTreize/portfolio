16685723083199e1762461d49b1879f5df8e78ef
deltaTreize <ludo.leblond13@gmail.com>
2024-06-13 11:31:13 +0200

    fix: correction conflit

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -11,7 +11,6 @@ export function Menu() {
 				<li className="menuToggle" onClick={() => setIsActive(!isActive)}>
 					{!isActive && <p>open</p>}
 					{isActive && <p>close</p>}
-					
 				</li>
 				<li style={{ '--i': 0, '--color': 'red', '--img': 'url("/assets/booki.webp")' } as React.CSSProperties}>
 					{" "}
@@ -31,23 +30,17 @@ export function Menu() {
 						rel="noreferrer"
 						href="https://deltatreize.github.io/projet4/homePage.html"
 						aria-label="lien vers projet ohmyfood.com"
-
 					>
 					<p className="title">OhMyFood</p>
 					</a>
 				</li>
-<<<<<<< HEAD
 				<li style={{ '--i': 2, '--color': 'yellow', '--img': 'url("/assets/sophieBuel.png")' } as React.CSSProperties}>
-=======
-				<li style={{ '--i': 2, '--color': 'yellow', '--img': 'url("/assets/sophiebuel.webp")' } as React.CSSProperties}>
->>>>>>> feature_branch
 					{" "}
 					<a
 						target="_blank"
 						rel="noreferrer"
 						href="https://deltatreize.github.io/projet6/FrontEnd/index.html"
 						aria-label="lien vers projet sophiebuel.com"
-
 					>
 					<p className="title">Sophie Buel</p>
 					</a>
@@ -59,7 +52,6 @@ export function Menu() {
 						rel="noreferrer"
 						href="https://p8-kasa-umber.vercel.app/"
 						aria-label="lien vers projet kasa.com"
-
 					>
 					<p className="title">Kasa.com</p>
 					</a>
@@ -71,7 +63,6 @@ export function Menu() {
 						rel="noreferrer"
 						href="https://p11-three.vercel.app/"
 						aria-label="lien vers projet ludobank.com"
-
 					>
 					<p className="title">LudoBank</p>
 					</a>
@@ -83,7 +74,6 @@ export function Menu() {
 						rel="noreferrer"
 						href="https://harry-potter-react-three.vercel.app/"
 						aria-label="lien vers projet harrypotter"
-
 					>
 					<p className="title">Harry Potter</p>
 					</a>
