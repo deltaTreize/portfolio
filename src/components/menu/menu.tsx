aae21fd8b9575dcf8a61d1a091ab05b7b8ea2285
deltaTreize <ludo.leblond13@gmail.com>
2024-06-13 11:33:43 +0200

    fix: faute de frappe img

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -34,7 +34,7 @@ export function Menu() {
 					<p className="title">OhMyFood</p>
 					</a>
 				</li>
-				<li style={{ '--i': 2, '--color': 'yellow', '--img': 'url("/assets/sophieBuel.png")' } as React.CSSProperties}>
+				<li style={{ '--i': 2, '--color': 'yellow', '--img': 'url("/assets/sophieBuel.webp")' } as React.CSSProperties}>
 					{" "}
 					<a
 						target="_blank"
