d3cc223653b4c2162332ecbe034ae659f72af9e0
deltaTreize <ludo.leblond13@gmail.com>
2025-01-29 17:01:10 +0100

    feat: update projets refactor code

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,72 +1,22 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import projets from "../../Projets.json";
 import "./menu.scss";
 
 export function Menu() {
-	const [isActive, setIsActive] = useState(false);
-
 	return (
 		<div className="menu-wrapper">
 			<h2>PROJETS</h2>
-			<ul className={`menu ${isActive ? "active" : ""}`}>
-				<li className="menuToggle" onClick={() => setIsActive(!isActive)}>
-					{!isActive && <p>open</p>}
-					{isActive && <p>close</p>}
-				</li>
-				<li style={{ '--i': 0, '--color': 'red', '--img': 'url("/assets/candyshop.png")' } as React.CSSProperties}>
-					{" "}
-					<Link
-						to={"/candyshop-by-stess.fr"}
-						aria-label="lien vers projet candyshop-by-stess.fr"
-					>
-					<p className="title" style={{fontSize: "0.7vw"}}>Candyshop-by-stess.fr</p>
-					</Link>
-				</li>
-				<li style={{ '--i': 1, '--color': '#7451eb', '--img': 'url("/assets/ohmyfood.webp")' } as React.CSSProperties} className="rotate">
-					{" "}
-					<Link
-						to={"/ohmyfood"}
-						aria-label="lien vers projet ohmyfood.com"
-					>
-					<p className="title">OhMyFood</p>
-					</Link>
-				</li>
-				<li style={{ '--i': 2, '--color': '#7451eb', '--img': 'url("/assets/sophieBuel.webp")' } as React.CSSProperties}>
-					{" "}
-					<Link
-						to={"/sophieBuel"}
-						aria-label="lien vers projet sophiebuel.com"
-					>
-					<p className="title">Sophie Buel</p>
-					</Link>
-				</li>
-				<li style={{ '--i': 3, '--color': '#7451eb', '--img': 'url("/assets/kasa.webp")' } as React.CSSProperties} className="rotate">
-					{" "}
-					<Link
-						to={"/Kasa"}
-						aria-label="lien vers projet kasa.com"
-					>
-					<p className="title">Kasa</p>
-					</Link>
-				</li>
-				<li style={{ '--i': 4, '--color': 'red', '--img': 'url("/assets/ludobank.webp")' } as React.CSSProperties}>
-					{" "}
-					<Link
-						to={"/LudoBank"}
-						aria-label="lien vers projet ludobank.com"
-					>
-					<p className="title">LudoBank</p>
-					</Link>
-				</li>
-				<li style={{ '--i': 5, '--color': 'red', '--img': 'url("/assets/harryPotter.webp")' } as React.CSSProperties} className="rotate">
-					{" "}
-					<Link
-						to={"/HarryPotter"}
-						aria-label="lien vers projet harrypotter"
-					>
-					<p className="title">Harry Potter</p>
-					</Link>
-				</li>
+			<ul className="menu">
+				{projets.map((projet) => (
+					<li key={projet.id} style={{border: `2px solid ${projet.color}`}}>
+						<h3>{projet.name}</h3>
+						<NavLink
+							to={`/${projet.name}`}
+							style={{ backgroundImage: `url(${projet.pictureHome})` }}
+						></NavLink>
+					</li>
+				))}
 			</ul>
 		</div>
 	);
--- src/pages/acceuil/acceuil.tsx
+++ src/pages/acceuil/acceuil.tsx
@@ -2,52 +2,65 @@ import React from "react";
 import { About } from "../../components/about/about.tsx";
 import { Menu } from "../../components/menu/menu.tsx";
 import { Technos } from "../../components/technos/technos.tsx";
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from "react-helmet-async";
 import "./acceuil.scss";
 
 export function Acceuil() {
 	const jsonLd = {
-    "@context": "https://schema.org",
-    "@type": "WebPage",
-    "name": "Page d'accueil du Portfolio de Ludovic LEBLOND",
-    "url": "https://ludovic-leblond.fr",
-    "description": "Portefolio de Ludovic LEBLOND, developpeur/integrateur WEB",
-    "publisher": {
-      "@type": "Organization",
-      "name": "LEBLOND Ludovic",
-      "url": "https://ludovic-leblond.fr"
-    },
-    "image": "https://ludovic-leblond.fr/assets/moi.webp",
-  };
+		"@context": "https://schema.org",
+		"@type": "ProfilePage",
+		name: "Page d'accueil du Portfolio de Ludovic LEBLOND",
+		url: "https://ludovic-leblond.fr",
+		description: "Portfolio de Ludovic LEBLOND, Développeur/Intégrateur Web spécialisé en React et TypeScript. Découvrez mes projets et compétences.",
+		publisher: {
+			"@type": "Person",
+			name: "Ludovic LEBLOND",
+			url: "https://ludovic-leblond.fr",
+		},
+		image: "https://ludovic-leblond.fr/assets/moi.webp",
+	};
 	return (
 		<>
-		<Helmet>
-        <title>Portefolio de Ludovic LEBLOND</title>
-        <meta name="description" content="Portefolio de Ludovic LEBLOND, developpeur/integrateur WEB" />
-        
-        {/* Balises Open Graph */}
-        <meta property="og:title" content="Portefolio de Ludovic LEBLOND" />
-        <meta property="og:description" content="Portefolio de Ludovic LEBLOND, developpeur/integrateur WEB" />
-        <meta property="og:image" content="https://ludovic-leblond.fr/assets/moi.webp" />
-        <meta property="og:url" content="https://ludovic-leblond.fr" />
-        
-        {/* Balises Twitter */}
-        <meta name="twitter:title" content="Portefolio de Ludovic LEBLOND" />
-        <meta name="twitter:description" content="Portefolio de Ludovic LEBLOND, developpeur/integrateur WEB" />
-        <meta name="twitter:image" content="https://ludovic-leblond.fr/assets/moi.webp" />
-        <meta name="twitter:card" content="summary_large_image" />
+			<Helmet>
+				<title>Portfolio de Ludovic LEBLOND</title>
+				<link rel="canonical" href="https://ludovic-leblond.fr" />
+				<meta
+					name="description"
+					content="Portfolio de Ludovic LEBLOND, Développeur/Intégrateur Web spécialisé en React et TypeScript. Découvrez mes projets et compétences."
+				/>
 
-        {/* JSON-LD */}
-        <script type="application/ld+json">
-          {JSON.stringify(jsonLd)}
-        </script>
-      </Helmet>
-		<div className="acceuil">
-			<h1>LEBLOND Ludovic</h1>
-			<Menu />
-			<About />
-			<Technos />
-		</div>
+				{/* Open Graph */}
+				<meta property="og:title" content="Portfolio de Ludovic LEBLOND" />
+				<meta
+					property="og:description"
+					content="Portfolio de Ludovic LEBLOND, Développeur/Intégrateur Web spécialisé en React et TypeScript. Découvrez mes projets et compétences."
+				/>
+				<meta property="og:type" content="website" />
+				<meta property="og:image" content="https://ludovic-leblond.fr/assets/moi.webp" />
+				<meta property="og:url" content="https://ludovic-leblond.fr" />
+				<meta property="og:locale" content="fr_FR" />
+
+				{/* Twitter */}
+				<meta name="twitter:title" content="Portfolio de Ludovic LEBLOND" />
+				<meta
+					name="twitter:description"
+					content="Portfolio de Ludovic LEBLOND, Développeur/Intégrateur Web spécialisé en React et TypeScript. Découvrez mes projets et compétences."
+				/>
+				<meta name="twitter:image" content="https://ludovic-leblond.fr/assets/moi.webp" />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@ton_twitter" /> {/* Remplace "@ton_twitter" par ton handle */}
+
+				{/* JSON-LD */}
+				<script type="application/ld+json">{JSON.stringify(jsonLd)}</script>
+			</Helmet>
+			<div className="acceuil">
+				<h1>LEBLOND Ludovic</h1>
+				<div className="acceuil-wrapper">
+					<About />
+					<Menu />
+					<Technos />
+				</div>
+			</div>
 		</>
 	);
 }
